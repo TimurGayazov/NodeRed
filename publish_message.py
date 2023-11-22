9ac743eccd5fd2fe012e4f5602d0071e2dab97c6
Timur Gayazov <tim.gayazov@gmail.com>
2023-11-22 23:16:00 +0400

    add 1.2 lab in readme.md

--- publish_message.py
+++ publish_message.py
@@ -6,8 +6,8 @@ mqtt_topic = "lamp_control_topic"
 
 # Сообщение для отправки
 message = {
-    "color": {"r": 255, "g": 0, "b": 255},
-    "brightness": 0
+    "color": {"r": 0, "g": 0, "b": 255},
+    "brightness": 100
 }
 
 # Опубликовать сообщение
