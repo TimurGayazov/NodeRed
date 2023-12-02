cf74ad884e0cad63825526238779dff6759531b6
Timur Gayazov <tim.gayazov@gmail.com>
2023-12-02 19:45:19 +0400

    added nfc door programm

--- NFC-door/nfc_door_lab.py
+++ NFC-door/nfc_door_lab.py
@@ -1,6 +1,7 @@
 import time
 import paho.mqtt.client as mqtt
 import sqlite3
+import threading
 db_name = 'door_logs.db'
 
 
@@ -12,14 +13,34 @@ class DoorLock:
     def unlock(self):
         self.is_locked = True
         print("Door is unlocked")
-        self.mqtt_client.publish("door_status", "unlocked")
+        self.mqtt_client.publish("door_status", "unlock")
+        self.mqtt_client.publish("onoff", "false")
         write_to_database('Unlock')
 
     def lock(self):
         self.is_locked = False
         print("Door is locked")
-        self.mqtt_client.publish("door_status", "locked")
+        self.mqtt_client.publish("door_status", "lock")
+        self.mqtt_client.publish("onoff", "true")
         write_to_database('Lock')
+
+    def auto_mode(self):
+        self.is_locked = True
+        print("Door is unlocked")
+        self.mqtt_client.publish("door_status", "unlock")
+        self.mqtt_client.publish("onoff", "false")
+        write_to_database('Unlock')
+
+        threading.Timer(5, self.complete_auto_mode).start()
+
+    def complete_auto_mode(self):
+        self.is_locked = False
+        print("Door is locked")
+        self.mqtt_client.publish("door_status", "lock")
+        self.mqtt_client.publish("onoff", "true")
+        write_to_database('Lock')
+
+
 
 
 def create_database():
@@ -38,7 +59,7 @@ def write_to_database(state):
     mas_data = [time.ctime(), state]
     cursor.execute("INSERT INTO data (timedate, state) VALUES (?, ?)", mas_data)
     connect.commit()
-
+    print(f'{state} было записано в базу данных')
 
 def on_connect(client, userdata, flags, rc):
     print(f"Connected with result code {rc}")
@@ -50,12 +71,10 @@ def on_message(client, userdata, msg):
     print(f"Received message: {msg.payload}")
     if msg.payload == b'unlock':
         door_lock.unlock()
-        time.sleep(10)
-        door_lock.lock()
     elif msg.payload == b'lock':
-        door_lock.unlock()
-        time.sleep(10)
         door_lock.lock()
+    elif msg.payload == b'auto-mode':
+        door_lock.auto_mode()
 
 
 mqtt_client = mqtt.Client()
